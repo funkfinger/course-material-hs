98ae5d9225307f49378266c4c7785096b8e5cd14
Jay Wiggins <5948+funkfinger@users.noreply.github.com>
2023-01-04 19:13:15 -0700

    new project...

---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>PCDS Coding as Art Information</h1>
+      <ul>
+        <li>
+          <a href="https://slides.com/funkfinger/coding-as-art-1">
+            First Days Slides - Coding as Art I
+          </a>
+        </li>
+        <li>
+          <a href="slides/first-days-caa2/slides-coding-as-art-2-first-days.html">
+            First Days Slides - Coding as Art II
+          </a>
+        </li>
+      </ul>
+    </main>
+  );
+};
+
+export default IndexPage;
+
+export const Head = () => (
+  <title>PCDS Coding as Art - Course Information</title>
+);
